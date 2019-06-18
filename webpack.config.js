50197b270d7a2cdd19f7f428c03241a5cd8397dd
HI <hi@HIs-iMac.local>
2019-06-18 14:04:17 +0500

    set port 3000

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   devServer: {
-    port: 30005,
+    port: 3000,
     open: true,
     proxy: {
       '/api': 'http://localhost:8080'
